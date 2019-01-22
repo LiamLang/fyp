b84c4cf289d73f3cc6af77e9f9a63193f92c3ad8
LiamLang <liammlang@gmail.com>
2019-01-22 23:31:41 +0000

    TransactionBuilder

--- src/main/java/com/liamlang/fyp/service/TransactionBuilder.java
+++ src/main/java/com/liamlang/fyp/service/TransactionBuilder.java
@@ -2,7 +2,10 @@ package com.liamlang.fyp.service;
 
 import com.liamlang.fyp.Model.Component;
 import com.liamlang.fyp.Model.ComponentInfo;
+import com.liamlang.fyp.Model.OwnershipChangeSignature;
 import com.liamlang.fyp.Model.Transaction;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class TransactionBuilder {
@@ -23,17 +26,68 @@ public class TransactionBuilder {
         return new Transaction(components, new ArrayList<>());
     }
     
-    public Transaction addComponetsToOther(Component parent, ArrayList<Component> children) {
+    public Transaction addComponetsToOther(Component parent, ArrayList<Component> childrenToAdd) throws Exception {
         
-        // get list of subcomponents from parent
+        ArrayList<Component> subcomponents = parent.getSubcomponents();
         
-        // add new children to it
+        ArrayList<Component> newComponents = new ArrayList<>();
+
+        for (Component child : childrenToAdd) {
+            
+            if (child.getQuantity() < 1) {
+                throw new Exception();
+            }
+            
+            subcomponents.add(child);
+            
+            Component newChild = new Component(child.getInfo(), child.getSubcomponents(), child.getQuantity() - 1, child.getOwner(), child.getOwnerPubKey());
+            
+            newComponents.add(newChild);
+        }
         
-        // create new parent
+        Component newParent = new Component(parent.getInfo(), subcomponents, parent.getQuantity(), parent.getOwner(), parent.getOwnerPubKey());
+                
+        newComponents.add(newParent);
+        
+        return new Transaction(newComponents, new ArrayList<>());
     }
         
-    // TODO divide components
+    public Transaction removeComponentsFromOther(Component parent, ArrayList<Component> childrenToRemove) throws Exception {
+        
+        ArrayList<Component> subcomponents = parent.getSubcomponents();
+        
+        ArrayList<Component> newComponents = new ArrayList<>();
+        
+        for (Component child : childrenToRemove) {
+            
+            if (!subcomponents.remove(child)) {
+                throw new Exception();
+            }
+            
+            Component newChild = new Component(child.getInfo(), child.getSubcomponents(), child.getQuantity() + 1, child.getOwner(), child.getOwnerPubKey());
+            
+            newComponents.add(newChild);
+        }
+        
+        Component newParent = new Component(parent.getInfo(), subcomponents, parent.getQuantity(), parent.getOwner(), parent.getOwnerPubKey());
+        
+        newComponents.add(newParent);
+        
+        return new Transaction(newComponents, new ArrayList<>());
+    }
     
-    // TODO change of ownership - involving signature
-    
+    public Transaction changeOwner(Component component, String newOwner, PublicKey newOwnerPubKey, PrivateKey oldOwnerPrivKey) {
+        
+        OwnershipChangeSignature signature = new OwnershipChangeSignature(component.getHash(), newOwnerPubKey, oldOwnerPrivKey);
+        
+        ArrayList<OwnershipChangeSignature> signatures = new ArrayList<>();
+        signatures.add(signature);
+        
+        Component newComponent = new Component(component.getInfo(), component.getSubcomponents(), component.getQuantity(), newOwner, newOwnerPubKey);
+        
+        ArrayList<Component> components = new ArrayList<>();
+        components.add(newComponent);
+        
+        return new Transaction(components, signatures);
+    }
 }

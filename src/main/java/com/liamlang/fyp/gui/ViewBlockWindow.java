4000f019fb859d46f2bfcb113771103d80efd3c9
LiamLang <liammlang@gmail.com>
2019-01-30 10:49:57 +0000

    Test adding icon

--- src/main/java/com/liamlang/fyp/gui/ViewBlockWindow.java
+++ src/main/java/com/liamlang/fyp/gui/ViewBlockWindow.java
@@ -3,7 +3,11 @@ package com.liamlang.fyp.gui;
 import com.liamlang.fyp.Model.Block;
 import com.liamlang.fyp.Utils.Utils;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -20,24 +24,32 @@ public class ViewBlockWindow {
         WindowBase window = new WindowBase("View Block");
         JPanel panel = window.getPanel();
 
+        try {
+            panel.add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/block.png")))));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        panel.add(Box.createRigidArea(new Dimension(0, 20)));
+
         panel.add(new JLabel("Hash: " + block.getHash()));
 
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
 
         panel.add(new JLabel("Previous hash: " + block.getPreviousHash()));
-        
+
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
 
         panel.add(new JLabel("Height: " + Integer.toString(block.getHeight())));
-        
+
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
 
-        // transactions
+        // transactions TODO
         
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
 
         panel.add(new JLabel("Timestamp: " + Utils.toHumanReadableTime(block.getTimestamp())));
-        
+
         window.show(600);
     }
 }
